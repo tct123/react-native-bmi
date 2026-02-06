accf6e61c9092ca1d3c62fffd4aa713942a70822
tct123 <42028373+tct123@users.noreply.github.com>
2026-02-06 17:49:46 +0100

    Use expo-device to set screen orientation
    Import expo-device and add helper functions to detect tablets and choose orientation. RootLayout now sets Stack screenOptions.orientation to "all" for tablets and "default" otherwise. Also add expo-device to package.json (and update lockfile).

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,26 @@
 import { Stack } from "expo-router";
+import { DeviceType } from "expo-device";
 
+function checktablet() {
+  if (DeviceType.TABLET) {
+    return true
+  } else {
+    return false
+  }
+}
+function setorientation() {
+  if (checktablet() === true) {
+    return "all"
+  } else {
+    return "default"
+  }
+}
 export default function RootLayout() {
   return <Stack screenOptions={{
     title: "BMI Calculator",
     headerStyle: {
       backgroundColor: "#FFCB1F"
-    }
+    },
+    orientation: setorientation()
   }} />;
 }
